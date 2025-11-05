cdeb8875b3cd37f680b2bbf98f4b4bd303f9159b
QuanTrLe <atl226@lehigh.edu>
2025-11-04 22:55:38 -0500

    basically just went through Warren's implementation to understand the details. This is a great visualizer but I'm gonna make a version that's specifically for benchmarking without the prints, as well some other stuffs like border padding thats not in here.

--- GameOfLife.cpp
+++ GameOfLife.cpp
@@ -30,20 +30,25 @@ void printGrid(const vector<vector<uint8_t>>& grid) {
 }
 
 int main() {
+    // size of the grid, as a square currently though
     int rows = 16, cols = 16;
+
+    // the array to store the entire lattice / grid in, specifically a 2D array for row and column
     vector<vector<uint8_t>> grid(rows, vector<uint8_t>(cols, 0));
 
+    // Currently setting the initial pattern by hand, bit dubious but eh it's jsut testing rn
     // Example: a blinker pattern
     grid[3][2] = 1;
     grid[3][3] = 1;
     grid[3][4] = 1;
 
     while (true) {
-        system("cls"); 
-        printGrid(grid);
+        system("cls"); // since we're printing the thing out everytime, clear for sake of mind
+        printGrid(grid); // print new grid out
 
-        vector<vector<uint8_t>> newGrid = grid;
+        vector<vector<uint8_t>> newGrid = grid; // make sure we update the grid we're using
 
+        // go through all the rows and columns
         for (int r = 0; r < rows; ++r) {
             for (int c = 0; c < cols; ++c) {
                 int n = countNeighbors(grid, r, c);
@@ -57,7 +62,7 @@ int main() {
         }
 
         grid.swap(newGrid);
-        this_thread::sleep_for(chrono::milliseconds(1000));
+        this_thread::sleep_for(chrono::milliseconds(1000)); // wait before we calc and print new grid
     }
 
     return 0;
